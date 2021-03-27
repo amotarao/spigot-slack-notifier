3c45aa739bbd833ffd6b650d06dbc087e0d63d60
Amon Sawamura <amon.sawamura@gmail.com>
2021-03-27 12:55:30 +0900

    registerEvents

--- src/main/java/dev/amotarao/spigot/slacknotifier/SlackNotifier.java
+++ src/main/java/dev/amotarao/spigot/slacknotifier/SlackNotifier.java
@@ -1,16 +1,26 @@
 package dev.amotarao.spigot.slacknotifier;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SlackNotifier extends JavaPlugin {
+public final class SlackNotifier extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        String playerName = e.getPlayer().getName();
+        e.setJoinMessage("こんにちは " + playerName + "!");
     }
 }
